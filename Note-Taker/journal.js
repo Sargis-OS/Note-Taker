20f4db088485735c69c81e89823741d731bdd0ad
Seggy0s <sargisshawel8@gmail.com>
2020-03-06 15:55:40 -0600

    final

---
+++ Note-Taker/journal.js
@@ -0,0 +1,5 @@
+[
+  { title: "tests", body: "dumb", journal: "will not pass" },
+  { title: "lifting", body: "sculpted", journal: "excercise" },
+  { title: "pizza", body: "fat", journal: "food" }
+];
